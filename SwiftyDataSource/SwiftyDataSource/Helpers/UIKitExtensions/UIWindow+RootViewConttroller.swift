af3f7dca56f992d82dcd9cc06916b0b5d180f551
Alexey Bakhtin <ai.bakhtin@gmail.com>
2019-09-30 18:33:45 +0300

    Fix ios 13 rootviewcontroller

--- SwiftyDataSource/SwiftyDataSource/Helpers/UIKitExtensions/UIWindow+RootViewConttroller.swift
+++ SwiftyDataSource/SwiftyDataSource/Helpers/UIKitExtensions/UIWindow+RootViewConttroller.swift
@@ -9,19 +9,19 @@
 import UIKit
 
 public extension UIWindow {
-    
+
     /// Fix for http://stackoverflow.com/a/27153956/849645
-    func setRootViewController(_ newRootViewController: UIViewController, withTransition transition: CATransition? = nil) {
-        
+    func set(rootViewController newRootViewController: UIViewController, withTransition transition: CATransition? = nil) {
+
         let previousViewController = rootViewController
-        
+
         if let transition = transition {
             // Add the transition
             layer.add(transition, forKey: kCATransition)
         }
-        
+
         rootViewController = newRootViewController
-        
+
         // Update status bar appearance using the new view controllers appearance - animate if needed
         if UIView.areAnimationsEnabled {
             UIView.animate(withDuration: CATransaction.animationDuration()) {
@@ -30,14 +30,17 @@ public extension UIWindow {
         } else {
             newRootViewController.setNeedsStatusBarAppearanceUpdate()
         }
-        
-        /// The presenting view controllers view doesn't get removed from the window as its currently transistioning and presenting a view controller
-        if let transitionViewClass = NSClassFromString("UITransitionView") {
-            for subview in subviews where subview.isKind(of: transitionViewClass) {
-                subview.removeFromSuperview()
+
+        if #available(iOS 13.0, *) {
+            // In iOS 13 we don't want to remove the transition view as it'll create a blank screen
+        } else {
+            // The presenting view controllers view doesn't get removed from the window as its currently transistioning and presenting a view controller
+            if let transitionViewClass = NSClassFromString("UITransitionView") {
+                for subview in subviews where subview.isKind(of: transitionViewClass) {
+                    subview.removeFromSuperview()
+                }
             }
         }
-        
         if let previousViewController = previousViewController {
             // Allow the view controller to be deallocated
             previousViewController.dismiss(animated: false) {
@@ -47,4 +50,3 @@ public extension UIWindow {
         }
     }
 }
-
